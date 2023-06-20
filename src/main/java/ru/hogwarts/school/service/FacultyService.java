6ad32ba1aebbe231cb2b79ac1922f3fb70301b91
alex_kalinychev <alex.kalinychev@yandex.ru>
2023-06-20 14:02:16 +0300

    Intermediate Commit

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,33 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class FacultyService {
+    private final HashMap<Long, Faculty> faculties = new HashMap<>();
+    private long lastId = 0;
+
+    public Faculty createFaculty(Faculty faculty) {
+        faculty.setId(++lastId);
+        faculties.put(lastId, faculty);
+        return faculty;
+    }
+
+    public Faculty findFaculty(long id) {
+        return faculties.get(id);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        if (faculties.containsKey(faculty.getId())) {
+            faculties.put(faculty.getId(), faculty);
+            return faculty;
+        }
+        return null;
+    }
+    public Faculty deleteFaculty(long id){
+        return faculties.remove(id);
+    }
+
+}
